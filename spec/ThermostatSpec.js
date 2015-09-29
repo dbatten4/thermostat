b1ca871d5d38d0dad00a4a61f6ebb9449afa0f59
Dom Batten <dominic.batten@googlemail.com>
2015-09-29 16:19:48 +0100

    Tests passing for power save mode, maxium and minimum temperatures

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -16,5 +16,44 @@ describe ('Thermostat', function(){
     expect(thermostat.temp).toEqual(19);
   });
 
+  it ("should have minimum temperature of 10 degrees", function(){
+    for(var i=0; i<10; i++){
+      thermostat.decrease();
+    };
+    expect(function() { thermostat.decrease(); }).toThrowError("Already at minimum temperature");
+  });
+
+  it ("should have power saving mode on by default", function(){
+    expect(thermostat.powerSave).toBe(true);
+  });
+
+  it ("should have max temp of 25 degrees if power saving mode is on", function(){
+    for(var i=0; i<5; i++){
+      thermostat.increase();
+    };
+    expect(function() { thermostat.increase(); }).toThrowError("Already at maximum temperature");
+  });
+
+  it ("can turn off power saving mode", function(){
+    thermostat.switchPowerSave();
+    expect(thermostat.powerSave).toBe(false);
+  });
+
+  it ("can turn on power saving mode", function(){
+    thermostat.switchPowerSave();
+    thermostat.switchPowerSave();
+    expect(thermostat.powerSave).toBe(true);
+  });
+
+  it ("should have max temp of 32 degrees if power saving mode is off", function(){
+    thermostat.switchPowerSave();
+    for(var i=0; i<12; i++){
+      thermostat.increase();
+    };
+    console.log(thermostat.temp);
+    console.log(thermostat.powerSave);
+    expect(function() { thermostat.increase(); }).toThrowError("Already at maximum temperature");
+  });
+
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,11 +1,22 @@
 Thermostat = function(){
   this.temp = 20;
+  this.powerSave = true;
 };
 
 Thermostat.prototype.increase = function(){
-  this.temp++
+  if((this.powerSave == true && this.temp >= 25) || (this.powerSave == false && this.temp >= 32)) {
+    throw new Error("Already at maximum temperature");
+  };
+  this.temp++;
 };
 
 Thermostat.prototype.decrease = function(){
-  this.temp--
+  if(this.temp <= 10) {
+    throw new Error("Already at minimum temperature");
+  };
+  this.temp--;
+};
+
+Thermostat.prototype.switchPowerSave = function(){
+  this.powerSave = !this.powerSave;
 };
